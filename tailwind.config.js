327e06037ab0a0fde4c9d4dfdb2049eccfd67d6e
Tyler Bratton <tylerbratton96@gmail.com>
2021-04-26 23:01:38 -0400

    fix tailwind config so that team colors aren't purged

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  purge: { content: ['./public/**/*.html', './src/**/*.vue', './src/**/*.js'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
