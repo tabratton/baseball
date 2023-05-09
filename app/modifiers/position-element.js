504675fcfde42aca28dc1bd6bf9f6abc554be390
Tyler Bratton <tylerbratton96@gmail.com>
2023-05-08 20:55:39 -0400

    slightly smarter tooltip positioning for diff chart

---
+++ app/modifiers/position-element.js
@@ -0,0 +1,20 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier(function positionElement(
+  element,
+  positional,
+  { top, left }
+) {
+  element.style.top = `${top}px`;
+  element.style.left = `${left}px`;
+
+  const width = element.offsetWidth;
+  const tooltipEnd = width + left;
+
+  if (
+    tooltipEnd >
+    element.parentElement.offsetWidth + element.parentElement.offsetLeft - 32
+  ) {
+    element.style.left = `${left - width}px`;
+  }
+});
