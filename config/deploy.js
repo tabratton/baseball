f45a7fdace4904dd5e186dcf4d3b8821f8b0c621
Tyler Bratton <tylerbratton96@gmail.com>
2025-01-04 18:49:23 -0500

    update ember-cli-deploy

--- config/deploy.js
+++ config/deploy.js
@@ -16,17 +16,17 @@ module.exports = function (deployTarget) {
   };
 
   if (deployTarget === 'production') {
-    ENV.s3.accessKeyId = process.env.AWS_KEY;
-    ENV.s3.secretAccessKey = process.env.AWS_SECRET;
+    ENV.s3.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    ENV.s3.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
     ENV.s3.bucket = process.env.BUCKET;
     ENV.s3.region = process.env.REGION;
-    ENV['s3-index'].accessKeyId = process.env.AWS_KEY;
-    ENV['s3-index'].secretAccessKey = process.env.AWS_SECRET;
+    ENV['s3-index'].accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    ENV['s3-index'].secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
     ENV['s3-index'].bucket = process.env.BUCKET;
     ENV['s3-index'].region = process.env.REGION;
     ENV.cloudfront = {
-      accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
       distribution: process.env.CLOUDFRONT_DISTRIBUTION,
     };
   }
