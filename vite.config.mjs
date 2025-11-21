8a480c8aee6599059344c9181eb76b37bcc7bdab
Tyler Bratton <tylerbratton96@gmail.com>
2025-11-20 20:12:30 -0500

    update dependencies

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
+import { defineConfig } from 'vite';
+
+import { browsers as targets } from './config/targets.js';
 
 export default defineConfig({
+  build: {
+    cssMinify: 'lightningcss',
+  },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist(targets)),
+      minify: true,
+    },
+  },
   plugins: [
     classicEmberSupport(),
     ember(),
